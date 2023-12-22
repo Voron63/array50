1c6a6c79ce2d502497f4117bce74e0fea2ad3afa
Voron63 <154591119+Voron63@users.noreply.github.com>
2023-12-22 15:55:56 +0400

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            Console.Write("Введите число от 0 до 30: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            int b = 0;
+            int[] intsArray = new int[50];
+
+            if (choice > 30 || choice < 0)
+            {
+                Console.WriteLine("Ошибка");
+                return;
+            }
+
+            for (int i = 0; i < intsArray.Length; i++)
+            {
+                intsArray[i] = new Random().Next(0, 31);
+            }
+
+            for (int i = 0; i < intsArray.Length; i++)
+            {
+                if (choice == intsArray[i])
+                {
+                    Console.WriteLine("Да, это число есть в массиве");
+                    return;
+                }
+            }
+            Console.WriteLine("Нет, этого числа нет в массиве");
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Ошибка");
+            return;
+        }
+    }
+}
